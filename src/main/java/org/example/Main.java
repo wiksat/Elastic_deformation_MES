8028e8fc61b3cb2cca51262533be9428f1025243
wiksat <84770801+wiksat@users.noreply.github.com>
2023-01-24 18:49:04 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+import org.example.gui.App;
+import javafx.application.Application;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Elastic deformation MES by WikSat");
+        Application.launch(App.class, args);
+    }
+}
---
+++ src/main/java/org/example/gui/App.java
@@ -0,0 +1,10 @@
+package org.example.gui;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
